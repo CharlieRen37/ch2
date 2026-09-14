7aa246c86dece147dd1770a9d6a0887483bc0ed6
CharlieRen37 <charlieren2009@gmail.com>
2026-09-14 13:26:23 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,31 @@
+public class Date{
+
+
+
+
+	public static void main(String[] args){
+		String day= "Tuesday";
+		int date= 14;
+		String month= "September";
+		int year= 2026; 
+		System.out.print("American format: ");
+		System.out.print(day);
+		System.out.print(", ");
+		System.out.print(month);
+		System.out.print(" ");
+		System.out.print(day);
+		System.out.print(", ");
+		System.out.println(year);
+		
+		
+		
+		System.out.print("European format: ");
+		System.out.print(day);
+		System.out.print(" ");
+		System.out.print(date);
+		System.out.print(" ");
+		System.out.print(month);
+		System.out.print(" ");
+		System.out.println(year);
+	}
+}
